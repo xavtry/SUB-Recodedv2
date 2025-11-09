94135a5ec25d2e6836db8b7dc3324fc0ec5abb51
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:12:39 -0500

    Update iframeManager.js

--- src/iframeManager.js
+++ src/iframeManager.js
@@ -1,1 +1,9 @@
+import { sanitizeURL } from './sanitizer.js';
+import { sendToProxy } from './proxyClient.js';
 
+export function loadURL(rawURL) {
+  const iframe = document.getElementById('browserFrame');
+  const cleanURL = sanitizeURL(rawURL);
+  const proxiedURL = sendToProxy(cleanURL);
+  iframe.src = proxiedURL;
+}
