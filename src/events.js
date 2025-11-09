28b0eca65068bb8c86d2d7e86094b355ec0feb0b
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:14:43 -0500

    Update events.js

--- src/events.js
+++ src/events.js
@@ -1,1 +1,32 @@
+import { goBack, goForward, getCurrentURL, saveSession } from './sessionManager.js';
+import { loadURL } from './iframeManager.js';
+import { updateAddressBar } from './uiRenderer.js';
 
+export function bindEvents() {
+  document.getElementById('backBtn').onclick = () => {
+    const url = goBack();
+    if (url) {
+      loadURL(url);
+      updateAddressBar(url);
+    }
+  };
+
+  document.getElementById('forwardBtn').onclick = () => {
+    const url = goForward();
+    if (url) {
+      loadURL(url);
+      updateAddressBar(url);
+    }
+  };
+
+  document.getElementById('reloadBtn').onclick = () => {
+    const url = getCurrentURL();
+    if (url) loadURL(url);
+  };
+
+  document.getElementById('goBtn').onclick = () => {
+    const url = document.getElementById('urlInput').value;
+    saveSession(url);
+    loadURL(url);
+  };
+}
