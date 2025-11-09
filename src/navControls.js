903aae6fe270fcf9b9984d695308cb0ac5855bc0
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:12:25 -0500

    Update navControls.js

--- src/navControls.js
+++ src/navControls.js
@@ -1,1 +1,12 @@
+export function setupNavigation(loadURL) {
+  const urlInput = document.getElementById('urlInput');
+  const goBtn = document.getElementById('goBtn');
+  const backBtn = document.getElementById('backBtn');
+  const forwardBtn = document.getElementById('forwardBtn');
+  const reloadBtn = document.getElementById('reloadBtn');
 
+  goBtn.onclick = () => loadURL(urlInput.value);
+  backBtn.onclick = () => history.back();
+  forwardBtn.onclick = () => history.forward();
+  reloadBtn.onclick = () => location.reload();
+}
