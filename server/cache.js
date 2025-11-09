c687c65b5714c254d24a368b8ae78de1094eca0e
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:15:33 -0500

    Update cache.js

--- server/cache.js
+++ server/cache.js
@@ -1,1 +1,11 @@
+const cache = new Map();
 
+function cacheGet(url) {
+  return cache.get(url);
+}
+
+function cacheSet(url, content) {
+  cache.set(url, content);
+}
+
+module.exports = { cacheGet, cacheSet };
