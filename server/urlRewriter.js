1a5d183f6e88fca53d5bc9c969655a379336e42e
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:15:12 -0500

    Update urlRewriter.js

--- server/urlRewriter.js
+++ server/urlRewriter.js
@@ -1,1 +1,11 @@
+function rewriteHTML(html, baseURL) {
+  return html.replace(/(href|src)="(.*?)"/g, (match, attr, url) => {
+    if (url.startsWith('http') || url.startsWith('//')) {
+      return `${attr}="/proxy?url=${encodeURIComponent(url)}"`;
+    }
+    const absolute = new URL(url, baseURL).href;
+    return `${attr}="/proxy?url=${encodeURIComponent(absolute)}"`;
+  });
+}
 
+module.exports = { rewriteHTML };
