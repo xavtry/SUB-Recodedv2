f0110e64546ccb413a13ae2083d7052e705544bd
xavtry <sebastianishot68@gmail.com>
2025-11-08 19:12:48 -0500

    Update sanitizer.js

--- src/sanitizer.js
+++ src/sanitizer.js
@@ -1,1 +1,9 @@
-
+export function sanitizeURL(url) {
+  try {
+    const parsed = new URL(url.startsWith('http') ? url : `https://${url}`);
+    return parsed.href;
+  } catch {
+    alert('Invalid URL');
+    return '';
+  }
+}
